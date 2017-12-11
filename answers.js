ec76deeeefaf1182603d0ffd981474aa5942f8dc
Lillian Chernin <l.smith1654@gmail.com>
2017-12-11 17:02:24 -0600

    1st commit - selecting datatypes

---
+++ answers.js
@@ -0,0 +1,47 @@
+// Answers for Datatypes
+// 1.
+//Boolean.
+const lightSwitch = true;
+
+// 2.
+// String:
+const userEmail = "user@emaildomain.com";
+
+// Or a key/value pair on an object (with the value equal to a string representing
+// the email address):
+const user = {
+  emailAddress: "user@emaildomain.com";
+}
+
+// 3.
+// Array (becuase I don't know how I would arrange the "hull", "laser blast" as key/value properties):
+const spaceship = [
+  "hull",
+  "laser blasters",
+  "tractor beam",
+  "warp drive"
+]
+
+// 4.
+// Array:
+const studentNames = [
+  "Lillian",
+  "Fred",
+  "Tim"
+]
+
+// 5.
+// Array of objects:
+const studentNames2 = [
+  {name: "Lillian", location: "Austin, TX"},
+  {name: "Fred", location: "Nashville, TN"},
+  {name: "Tim", location: "Nashville, TN"}
+]
+// 6.
+// Array of objects with a nested array on the objects for the list of fav tv shows:
+
+const studentNames3 = [
+  {name: "Lillian", location: "Austin, TX", favTvShows: []},
+  {name: "Fred", location: "Nashville, TN", favTvShows: ["Murder She Wrote", "Stranger Things"]},
+  {name: "Tim", location: "Nashville, TN", favTvShows: ["60 Minutes", "Law and Order"]}
+]
